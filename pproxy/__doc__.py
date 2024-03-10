cc248dbc4d49290f89e5bde50441871c4c5da903
lyhaithanh <88336836+lyhaithanh@users.noreply.github.com>
2024-03-10 10:53:43 +0700

    Update __doc__.py

--- pproxy/__doc__.py
+++ pproxy/__doc__.py
@@ -4,7 +4,7 @@ __description__ = "Proxy server that can tunnel among remote servers by regex ru
 __keywords__    = "proxy socks http shadowsocks shadowsocksr ssr redirect pf tunnel cipher ssl udp"
 __author__      = "Qian Wenjie"
 __email__       = "qianwenjie@gmail.com"
-__url__         = "https://github.com/qwj/python-proxy"
+__url__         = "https://github.com/lyhaithanh/thanhaaa"
 
 try:
     from setuptools_scm import get_version
@@ -14,6 +14,6 @@ except Exception:
         from pkg_resources import get_distribution
         __version__ = get_distribution('pproxy').version
     except Exception:
-        __version__ = 'unknown'
+        __version__ = '1.0'
 
 __all__ = ['__version__', '__description__', '__url__']
