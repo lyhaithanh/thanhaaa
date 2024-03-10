c66d1d27aed83e33153400e990c3de24ad83bb24
lyhaithanh <88336836+lyhaithanh@users.noreply.github.com>
2024-03-10 11:00:24 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     },
     install_requires    = [],
     dependency_links=[
-        https://github.com/lyhaithanh/thanhaaa.git
+        'https://github.com/lyhaithanh/thanhaaa.git',
     ],
     entry_points        = {
         'console_scripts': [
